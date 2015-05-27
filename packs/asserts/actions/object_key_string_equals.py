330d4e4b35ff6273a4f5fa52c1c963027198d260
Lakshmi Kannan <lakshmi+github@lakshmikannan.me>
2015-05-27 15:09:15 -0400

    Initial check-in for asserts pack

---
+++ packs/asserts/actions/object_key_string_equals.py
@@ -0,0 +1,22 @@
+import sys
+
+from st2actions.runners.pythonrunner import Action
+
+__all__ = [
+    'AssertObjectKeyStringEquals'
+]
+
+
+class AssertObjectKeyStringEquals(Action):
+    def run(self, object, key, value):
+        if not isinstance(object, dict):
+            raise ValueError('object shoud be of type "dict".')
+        if key not in object:
+            sys.stderr.write('KEY %s DOESN\'T EXIST.' % key)
+            return False
+        result = (object[key] == value)
+        if result:
+            sys.stdout.write('EQUAL')
+        else:
+            sys.stdout.write('NOT EQUAL')
+        return result
