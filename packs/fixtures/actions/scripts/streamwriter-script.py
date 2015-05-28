c53be7944f48643e09799893dd6f33398c90a55b
Lakshmi Kannan <lakshmi+github@lakshmikannan.me>
2015-05-28 10:05:23 -0700

    Include a remote script fixture for testing

---
+++ packs/fixtures/actions/scripts/streamwriter-script.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import argparse
+import sys
+import ast
+
+from lib.exceptions import CustomException
+
+
+class StreamWriter(object):
+
+    def run(self, stream):
+        if stream.upper() == 'STDOUT':
+            sys.stdout.write('STREAM IS STDOUT.')
+            return stream
+
+        if stream.upper() == 'STDERR':
+            sys.stderr.write('STREAM IS STDERR.')
+            return stream
+
+        raise CustomException('Invalid stream specified.')
+
+
+def main(args):
+    stream = args.stream
+    writer = StreamWriter()
+    stream = writer.run(stream)
+    str_arg = args.str_arg
+    int_arg = args.int_arg
+    obj_arg = args.obj_arg
+    if str_arg:
+        sys.stdout.write(' STR: %s' % str_arg)
+    if int_arg:
+        sys.stdout.write(' INT: %d' % int_arg)
+    if obj_arg:
+        sys.stdout.write(' OBJ: %s' % obj_arg)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('--stream', help='Stream.', required=True)
+    parser.add_argument('--str_arg', help='Some string arg.')
+    parser.add_argument('--int_arg', help='Some int arg.', type=float)
+    parser.add_argument('--obj_arg', help='Some dict arg.', type=ast.literal_eval)
+    args = parser.parse_args()
+    main(args)
