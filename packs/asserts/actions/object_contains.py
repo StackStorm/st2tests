9a1ef96fb3830471cb766383fdc4684516d1871d
Patrick Hoolboom <patrick@stackstorm.com>
2016-06-23 20:43:51 +0000

    Fixed

--- packs/asserts/actions/object_contains.py
+++ packs/asserts/actions/object_contains.py
@@ -10,12 +10,18 @@ __all__ = [
 
 class AssertObjectContains(Action):
     def run(self, object, expected):
-        for item in expected:
-            if hasattr(object, item):
-                if not expected[item] == object[item]:
-                    pprint.pprint('Input: \n%s:%s' % (item, object[item]), stream=sys.stderr)
-                    pprint.pprint('Expected: \n%s:%s' % (item, expected[item]), stream=sys.stderr)
+        for obj in expected:
+            input_obj = object[obj]
+            expected_obj = expected[obj]
+            for item in expected_obj:
+                if not item in input_obj:
+                    pprint.pprint('Key not found: %s' % item, stream=sys.stderr)
                     raise ValueError('Objects not equal. Input: %s, Expected: %s.' % (object, expected))
                 else:
-                    sys.stdout.write('EQUAL.')
+                    if expected_obj[item] != input_obj[item]:
+                        pprint.pprint('Input: \n%s:%s' % (item, input_obj[item]), stream=sys.stderr)
+                        pprint.pprint('Expected: \n%s:%s' % (item, expected_obj[item]), stream=sys.stderr)
+                        raise ValueError('Objects not equal. Input: %s, Expected: %s.' % (object, expected))
+                    
+        sys.stdout.write('EQUAL.')
 
